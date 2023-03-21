751bd435eed442bbdabd7675b3068792e12a88af
joe2773 <jbarrymarron@gmail.com>
2023-03-22 08:54:07 +1300

    adding base model logic

--- learn.py
+++ learn.py
@@ -5,6 +5,7 @@ from linearRegression import LinearRegression
 learning_rate = 0.01
 num_epochs = 100
 param_info = dict({'linReg1',[1,2,3,4]},{'biasAdd1',{1,1,1,1}})
+input_data = []
 linearRegressionOp = LinearRegression();
 
 class Model(object):
@@ -13,13 +14,19 @@ class Model(object):
 
     def main(self):
         for i in range(num_epochs):
-            output = self.network_forward()
-            gradients = self.network_backward()
+            outputs = self.network_forward()
+            loss = self.lossOp.loss()
+            loss_grad = self.lossOp.loss_grad()
+            param_gradients = self.network_backward(loss_grad)
+            self.param_info = self.optimiser.optimise(param_gradients,self.param_info)
 
 
+    def network_forward(self, inputs: ndarray):
+        output = linearRegressionOp._forward(inputs)
+        return output
 
-    def network_forward(self):
-
-
-
-    def network_backward(self):
+    def network_backward(self,loss_grad: ndarray):
+        output_grad = linearRegressionOp.backward(loss_grad)
+        self.param_info['linReg1Grads'] = linearRegressionOp.param_grad()
+        return self.param_info
+    
